3e024d136f708530ca8290bbc5abca0bdfae124f
Dmitry Sluch <dmitry.sluch@yandex.ru>
2017-04-17 20:02:44 +0300

    added_file_parsing

--- IPcalc/main.cpp
+++ IPcalc/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <iomanip>
 #include <string>
+#include <fstream>
 #include "IPv4.h"
 using namespace std;
 
@@ -78,6 +78,86 @@ int main()
 			cin >> pr;
 			a &= IPv4::PrMask(pr);
 			cout << a;
+		}
+		else if (cmd == "open_file")
+		{
+			string path_in, path_out;
+			cin >> path_in >> path_out;
+			ifstream fin(path_in); ofstream fout(path_out);
+			while (!fin.eof())
+			{
+				cmd = "";
+				fin >> cmd;
+				if (cmd == "max_mask")
+				{
+					IPv4 a, b;
+
+					fin >> a >> b;
+					fout << IPv4::MaxMask(a, b);
+				}
+				else if (cmd == "max_net.id")
+				{
+					IPv4 a, b;
+					fin >> a >> b;
+
+					a = IPv4::MaxMask(a, b);
+					b &= a;
+					fout << b;
+				}
+				else if (cmd == "prefix_to_mask")
+				{
+					unsigned p; IPv4 m;
+					fin >> p;
+					m = IPv4::PrMask(p);
+					fout << m;
+				}
+				else if (cmd == "prefix_classes")
+				{
+					IPv4 a;
+					fin >> a;
+
+					unsigned pr = IPv4::PrClasses(a);
+					if (pr != ULONG_MAX) fout << pr << endl;
+				}
+				else if (cmd == "mask_classes")
+				{
+					IPv4 a;
+					fin >> a;
+
+					unsigned pr = IPv4::PrClasses(a);
+					if (pr != ULONG_MAX)
+					{
+						a = IPv4::PrMask(pr);
+						fout << a;
+					}
+				}
+				else if (cmd == "net.id_classes")
+				{
+					IPv4 a;
+					fin >> a;
+
+					unsigned pr = IPv4::PrClasses(a);
+					if (pr != ULONG_MAX)
+					{
+						a &= IPv4::PrMask(pr);
+						fout << a;
+					}
+				}
+				else if (cmd == "net.id_CIDR")
+				{
+					IPv4 a;
+					fin >> a;
+					unsigned pr;
+					fin >> pr;
+					a &= IPv4::PrMask(pr);
+					fout << a;
+				}
+				else if (cmd == "close")
+				{
+					break;
+				}
+			}
+			cout << "Done!" << endl;
 		}
 		else if(cmd == "exit")
 		{
